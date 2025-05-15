2aab1a0d3c8e246784b2533d66831f0b5a2a2034
Krasikoff <z1r0-d@yandex.ru>
2025-05-16 00:16:41 +0300

    Add selenium and click button in pop-up and location windows. Add API_URL.

--- competitors_parser/spiders/alkoteka.py
+++ competitors_parser/spiders/alkoteka.py
@@ -1,47 +1,87 @@
 from typing import Any, Dict, Iterator
-
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from scrapy import Request
 from scrapy.http import Response
 
 from .base import BaseCompetitorSpider
 
 ALL_URLS = [
-    'https://alkoteka.com/catalog/slaboalkogolnye-napitki-2', 
+    'https://alkoteka.com/catalog/slaboalkogolnye-napitki-2',
 ]
+API_URLS = (
+    'https://alkoteka.com/web-api/v1/product?city_uuid='
+    '4a70f9e0-46ae-11e7-83ff-00155d026416&'
+    'page=1&per_page=20&'
+    'root_category_slug=slaboalkogolnye-napitki-2'
+)
 
 
 class AlkotekaSpider(BaseCompetitorSpider):
     """Паук для парсинга сайта remex.ru."""
     name = 'alkoteka'
     allowed_domains = ['alkoteka.com']
-    start_urls = ['https://alkoteka.com/catalog/slaboalkogolnye-napitki-2']
+    start_urls = ['https://alkoteka.com/catalog/slaboalkogolnye-napitki-2',]
 
+    def __init__(self):
+        self.driver = webdriver.Firefox()
 
     def parse(self, response: Response) -> Iterator[Request]:
-        """Парсинг 3 url."""
-#        all_categories = response.css('a[href^="/price/"]')
+        """Парсинг url."""
+        self.driver.get(self.start_urls[0])
+        while True:
+            try:
+                time.sleep(3)
+                button_18 = self.driver.find_element(
+                    By.XPATH, 
+                    '//button[text()="Мне есть 18 лет"]'
+                )
+                button_18.click()
+                time.sleep(1)
+                button_change = self.driver.find_element(
+                    By.XPATH,
+                    '//button[normalize-space()="Изменить"]'
+                )
+                button_change.click()
+                time.sleep(1)
+                self.driver.find_element(
+                    By.XPATH,
+                    '//button[text()="Краснодар"]'
+                ).click()
+                time.sleep(2)
+                url = self.start_urls[0]
+                yield Request(url, callback=self.parse2)
+            except Exception as e:
+#                print(e)
+                break
+        self.driver.close()
 
-        for category in ALL_URLS:
-            category_url = category
-            category_name = category.css('h1::text').get()
+    def parse2(self, response):
+        print(response.text)
+        print(response.url)
+        category_name = response.xpath('//button[text()="Слабоалкогольные напитки"]')
+#        category_url = response.urls
+        print(category_name)
+            
+            # if not category_url:
+            #     continue
 
-            if not category_url:
-                continue
+        # if category_name:
+        #     category_name = category_name.capitalize()
 
-            if category_name:
-                category_name = category_name.capitalize()
+        # self.logger.info(
+        #     'Processing category: %s (%s)',
+        #     category_name,
+        #     category_url
+        # )
 
-            self.logger.info(
-                'Processing category: %s (%s)',
-                category_name,
-                category_url
-            )
-
-            yield Request(
-                url=response.urljoin(category_url),
-                callback=self.parse_category,
-                cb_kwargs={'category': category_name}
-            )
+        # yield Request(
+        #     url=response.urljoin(category_url),
+        #     callback=self.parse_category,
+        #     cb_kwargs={'category': category_name}
+        # )
 
     # def parse_category(
     #         self,
