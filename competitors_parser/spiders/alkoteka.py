ae9a96c8d867bd4c28b5439145fd47d4e2e85e0d
Krasikoff <z1r0-d@yandex.ru>
2025-05-17 00:40:07 +0300

    Add padination.

--- competitors_parser/spiders/alkoteka.py
+++ competitors_parser/spiders/alkoteka.py
@@ -1,4 +1,5 @@
 import json
+from collections import deque
 from typing import Iterator, Dict, Any, List
 import time
 from scrapy import Request
@@ -22,25 +23,33 @@ API_URL_PROD = (
     'city_uuid=4a70f9e0-46ae-11e7-83ff-00155d026416'
 )
 
+
 class AlkotekaSpider(BaseCompetitorSpider):
     """Паук для парсинга сайта remex.ru."""
     name = 'alkoteka'
     allowed_domains = ['alkoteka.com']
-    start_urls = ['https://alkoteka.com/catalog/slaboalkogolnye-napitki-2',]
+    start_urls = [
+        'https://alkoteka.com/catalog/slaboalkogolnye-napitki-2',
+        'https://alkoteka.com/catalog/krepkiy-alkogol',
+    ]
 
     # API URL шаблоны
     BASE_URL_CAT = 'https://alkoteka.com/catalog/'
     BASE_URL_PROD = 'https://alkoteka.com/product/'
-
     BASE_API_URL_BEGIN = 'https://alkoteka.com/web-api/v1/product'
-    BASE_API_CITY_UUID = '?city_uuid=4a70f9e0-46ae-11e7-83ff-00155d026416' # Краснодар
-    BASE_API_ITEM_ON_PAGE = '&page=1&per_page=20&'     # 1стр, 20 на стр
+    # Краснодар
+    BASE_API_CITY_UUID = '?city_uuid=4a70f9e0-46ae-11e7-83ff-00155d026416'
+    # 1стр, 20 на стр
+    BASE_API_PAGE_BEGIN = '&page='
+    BASE_API_PAGE_END = '&per_page=20&'
+    PAGE = 1
+    PAGES = {}
     BASE_API_URL_CAT = (
-        f'{BASE_API_URL_BEGIN}{BASE_API_CITY_UUID}{BASE_API_ITEM_ON_PAGE}'
+        f'{BASE_API_URL_BEGIN}{BASE_API_CITY_UUID}'
+        f'{BASE_API_PAGE_BEGIN}{PAGE}{BASE_API_PAGE_END}'
         'root_category_slug='
     )
     BY_STOCKS = False
-
 
     # Настройки для API запросов
     custom_settings = {
@@ -58,44 +67,49 @@ class AlkotekaSpider(BaseCompetitorSpider):
         self.processed_slugs = set()
         self.processed_ids = set()
 
-
     def parse(self, response: Response) -> Iterator[Request]:
         """Парсинг url."""
         title = response.css('title::text').get()
-        category_name = title.replace('купить в г.Краснодар  по доступной цене', '')
-        category_list_url = [url.replace(self.BASE_URL_CAT, self.BASE_API_URL_CAT) for url in self.start_urls]
+        category_name = title.replace(
+            'купить в г.Краснодар  по доступной цене', ''
+        )
+        category_list_url = [
+            url.replace(
+                self.BASE_URL_CAT, self.BASE_API_URL_CAT
+            ) for url in self.start_urls
+        ]
         for category_url in category_list_url:
+            category_slug = category_url.replace(self.BASE_API_URL_CAT, '')
+            self.PAGES[category_slug] = 1
             print(category_url)
-            print(category_name)            
+            print(category_name)
+            print(category_slug)
+
             yield Request(
                 url=category_url,
                 callback=self.parse_product_list,
-                cb_kwargs={'cat': category_name},
+                cb_kwargs={
+                    'cat': category_name,
+                    'cat_slug': category_slug,
+                },
                 dont_filter=True
             )
-
 
     def parse_product_list(
             self,
             response: Response,
-            cat: str
+            cat: str,
+            cat_slug: str
             ) -> Iterator[Request]:
         """Парсинг списка товаров в категории."""
         try:
             result = json.loads(response.body)
-#            print(result)
             products = result.get('results', [])
-#            print(products)
             self.logger.info(f'Найдено {len(products)} товаров в {cat}')
 
             for product in products:
                 product_slug = product.get('slug', '')
                 product_title = product.get('name', '')
-                # print(product_title)
-                # print(product_slug)
-
-
-
                 if not product_slug:
                     continue
 
@@ -109,24 +123,45 @@ class AlkotekaSpider(BaseCompetitorSpider):
                 # Добавляем slug в множество обработанных товаров
                 self.processed_slugs.add(product_slug)
 
-                url = f'{self.BASE_API_URL_BEGIN}/{product_slug}{self.BASE_API_CITY_UUID}'
+                url = (
+                    f'{self.BASE_API_URL_BEGIN}/{product_slug}'
+                    f'{self.BASE_API_CITY_UUID}'
+                )
 
                 yield Request(
                     url=url,
                     callback=self.parse_product,
                     cb_kwargs={
-                        'cat': f'{cat} --- {product_title}',
-                        'slug': f'{product_slug}'
+                        'cat': cat,
+                        'slug': product_slug
                     },
                     dont_filter=False
                 )
                 time.sleep(0.3)
-
         except json.JSONDecodeError as e:
             self.logger.error(f'Ошибка декодирования JSON: {str(e)}')
         except Exception as e:
             self.logger.error(f'Ошибка при обработке списка товаров: {str(e)}')
 
+        more_pages = result.get('meta', {})
+        if more_pages['has_more_pages']:
+            self.PAGES[cat_slug] += 1
+            print(f'has_more_pages, follow next page {self.PAGES[cat_slug]}')
+            next_page = (
+                f'{self.BASE_API_URL_BEGIN}{self.BASE_API_CITY_UUID}'
+                f'{self.BASE_API_PAGE_BEGIN}{self.PAGES[cat_slug]}'
+                f'{self.BASE_API_PAGE_END}'
+                f'root_category_slug={cat_slug}'
+            )
+            print(next_page)
+            yield response.follow(
+                next_page,
+                callback=self.parse_product_list,
+                cb_kwargs={
+                    'cat': cat,
+                    'cat_slug': cat_slug,
+                },
+            )
 
     def parse_product(
             self,
@@ -156,10 +191,10 @@ class AlkotekaSpider(BaseCompetitorSpider):
             subcategory_slug = subcategory['slug']
 
             # Формирование корректного URL для товара
-            product_url = f'{self.BASE_URL_PROD}{subcategory_slug}/{slug}'    # нужен еще слаг подкатегории
+            product_url = f'{self.BASE_URL_PROD}{subcategory_slug}/{slug}'
 
             # Получаем основную единицу измерения товара
-            filter_labels = product.get('filter_labels', [])            
+            filter_labels = product.get('filter_labels', [])
             main_unit = filter_labels[0].get('title', 'шт')
 
             # Получаем информацию о складах
@@ -169,12 +204,10 @@ class AlkotekaSpider(BaseCompetitorSpider):
                 stocks = []
                 amount = product.get('quantity_total', '')
                 stocks.append({
-                    'stock': f"Краснодар (Итого)",
+                    'stock': "Краснодар (Итого)",
                     'quantity': amount,
                     'price': main_price
                 })
-
-
 
             yield {
                 'category': cat,
@@ -184,10 +217,6 @@ class AlkotekaSpider(BaseCompetitorSpider):
                 'stocks': stocks,
                 'unit': main_unit,
                 'currency': 'RUB',
-                # 'weight': weight,
-                # 'length': length,
-                # 'width': width,
-                # 'height': None,
                 'url': product_url,
             }
             time.sleep(0.5)
@@ -196,7 +225,6 @@ class AlkotekaSpider(BaseCompetitorSpider):
             self.logger.error(f'Ошибка декодирования JSON: {str(e)}')
         except Exception as e:
             self.logger.error(f'Ошибка при обработке товара: {str(e)}')
-
 
     def _get_normalized_stocks(
             self,
